9b5b99cc4a3b0e0e65639f2ff28c4e837e700970
Mirko Abozzi <mirko.abozzi@gmail.com>
2024-07-15 18:08:58 +0200

    main reducer + action+ modale info profilo

--- src/redux/store/index.js
+++ src/redux/store/index.js
@@ -1,6 +1,9 @@
 import { combineReducers, configureStore } from "@reduxjs/toolkit";
+import MainReducer from "../reducers/MainReducer";
 
-const rootReducer = combineReducers({});
+const rootReducer = combineReducers({
+  mainReducer: MainReducer,
+});
 
 const store = configureStore({
   reducer: rootReducer,
