d214845fbcbdc7eed71177bdc86acad7c9d7bc9b
Mirko Abozzi <mirko.abozzi@gmail.com>
2024-07-17 22:41:24 +0200

    corretta responsive home

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -28,8 +28,6 @@ const Home = () => {
     dispatch(getPosts());
   }, [dispatch]);
 
-  // console.log("posts", posts);
-
   const dataConverter = (timeStamp) => {
     const data = new Date(timeStamp);
     return data.toLocaleString("it-it", {
@@ -59,16 +57,17 @@ const Home = () => {
     });
   };
 
+  console.log("posts", posts);
+
   return (
     <>
       <Container className="mt-4">
-        {/* Dispositivi Desktop */}
-        <Row className="d-none d-lg-flex">
+        <Row>
           <Col lg={3}>
             <HomeLeftBar />
           </Col>
-          <Col md={6}>
-            <h1>Posts</h1>
+          <Col lg={6}>
+            <h1>Home</h1>
             <Form onSubmit={handleSubmit}>
               <Form.Group className="mb-3" controlId="text">
                 <Form.Label>Aggiungi nuovo post</Form.Label>
@@ -78,100 +77,30 @@ const Home = () => {
             {isLoading ? (
               <Spinner animation="grow" />
             ) : (
-              [...posts].reverse().map((post) => {
-                return (
-                  <Card key={post._id} className="my-2">
-                    <Card.Body>
-                      <Card.Title>{post.user.username}</Card.Title>
-                      <Card.Text>{post.text}</Card.Text>
-                      <Card.Text>{dataConverter(post.createdAt)}</Card.Text>
-                      <Trash onClick={() => dispatch(deletePost(post._id))} />
-                      <Button className="ms-2" onClick={() => handleShow(post)}>
-                        Modifica
-                      </Button>
-                    </Card.Body>
-                  </Card>
-                );
-              })
+              [...posts]
+                .reverse()
+                .slice(0, 30)
+                .map((post) => {
+                  return (
+                    <Card key={post._id} className="my-2">
+                      <Card.Body>
+                        <div className="d-flex justify-content-between">
+                          <Card.Title>{post.user.username}</Card.Title>
+                          <Trash onClick={() => dispatch(deletePost(post._id))} />
+                        </div>
+                        <Card.Text>{post.text}</Card.Text>
+                        <Card.Text className="mb-0">Data creazione {dataConverter(post.createdAt)}</Card.Text>
+                        <Card.Text>Ultima modifica {dataConverter(post.updatedAt)}</Card.Text>
+                        <Button className="d-block mx-auto" onClick={() => handleShow(post)}>
+                          Modifica
+                        </Button>
+                      </Card.Body>
+                    </Card>
+                  );
+                })
             )}
           </Col>
-          <Col lg={3} className="d-none d-md-block">
-            <Notizie />
-            <HomeFooter />
-          </Col>
-        </Row>
-
-        {/* Dispositivi Medie dimensioni */}
-        <Row className="d-none d-md-flex d-lg-none">
-          <Col md={4}>
-            <HomeLeftBar />
-            <Notizie />
-            <HomeFooter />
-          </Col>
-          <Col md={8}>
-            <h1>Posts</h1>
-            <Form onSubmit={handleSubmit}>
-              <Form.Group className="mb-3" controlId="text">
-                <Form.Label>Aggiungi nuovo post</Form.Label>
-                <Form.Control type="text" placeholder="Scrivi qualcosa" value={post} onChange={(e) => setpost(e.target.value)} />
-              </Form.Group>
-            </Form>
-            {isLoading ? (
-              <Spinner animation="grow" />
-            ) : (
-              [...posts].reverse().map((post) => {
-                return (
-                  <Card key={post._id} className="my-2">
-                    <Card.Body>
-                      <Card.Title>{post.user.username}</Card.Title>
-                      <Card.Text>{post.text}</Card.Text>
-                      <Card.Text>{dataConverter(post.createdAt)}</Card.Text>
-                      <Trash onClick={() => dispatch(deletePost(post._id))} />
-                      <Button className="ms-2" onClick={() => handleShow(post)}>
-                        Modifica
-                      </Button>
-                    </Card.Body>
-                  </Card>
-                );
-              })
-            )}
-          </Col>
-        </Row>
-
-        {/* Dispositivi Mobile */}
-        <Row className="d-flex d-md-none">
-          <Col xs={12}>
-            <HomeLeftBar />
-          </Col>
-          <Col md={12}>
-            <h1>Posts</h1>
-            <Form onSubmit={handleSubmit}>
-              <Form.Group className="mb-3" controlId="text">
-                <Form.Label>Aggiungi nuovo post</Form.Label>
-                <Form.Control type="text" placeholder="Scrivi qualcosa" value={post} onChange={(e) => setpost(e.target.value)} />
-              </Form.Group>
-            </Form>
-            {isLoading ? (
-              <Spinner animation="grow" />
-            ) : (
-              [...posts].reverse().map((post) => {
-                return (
-                  <Card key={post._id} className="my-2">
-                    <Card.Body>
-                      <Card.Title>{post.user.username}</Card.Title>
-                      <Card.Text>{post.text}</Card.Text>
-                      <Card.Text>{dataConverter(post.createdAt)}</Card.Text>
-                      <Trash onClick={() => dispatch(deletePost(post._id))} />
-                      <Button className="ms-2" onClick={() => handleShow(post)}>
-                        Modifica
-                      </Button>
-                    </Card.Body>
-                  </Card>
-                );
-              })
-            )}
-          </Col>
-          <Col xs={12}>
+          <Col lg={3} className="d-none d-lg-block">
             <Notizie />
             <HomeFooter />
           </Col>
